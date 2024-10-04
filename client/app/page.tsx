ad9a172e99f3c41242c08e28089f926ea8660f7d
shaon99 <shaonahmed703@gmail.com>
2024-10-04 10:08:52 +0600

    shadcn ui added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <h1 className="text-center py-10 text-2xl">Scalable Chat Application 1.0.0</h1>;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-lg text-center py-3">
+        Scalable Chat Application Shadcn Ui
+      </h1>
+      <Button >Get Started</Button>
+    </div>
+  );
 }
